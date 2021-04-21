bb8034b81e6bb16bca25d6c9559680b68fdbe785
Alex Simzikov <uralban@gmail.com>
2021-04-21 17:26:38 +0300

    Change result list

--- app.js
+++ app.js
@@ -3,6 +3,13 @@
 	let app_el = document.getElementById('app');
 	let users_list_el = app_el.querySelector('.js_users_list');
 
+	function makeRender(selector) {
+	    let template = document.getElementById(selector).innerHTML;		
+	    return new Function('data', 'return `' + template + '`');
+	}
+
+	let renderUser = makeRender('users_list_template');
+
 	fetch('https://jsonplaceholder.typicode.com/users', {
 	  method: 'GET',
 	  headers: {
@@ -11,10 +18,7 @@
 	}).then((response) => response.json()).then((data) => {
 
 	  	let data_html = data.map(function(value) {
-            return `
-                <div class="js_user_item row">
-                    <div class="c5 js_item_text"><strong>Name: </strong>${value.name} <strong>Phone: </strong>${value.phone}</div>
-                </div>`;
+            return renderUser(value);
         }).join("");
 
         users_list_el.innerHTML = data_html;
